e21fa57a5d8a47c2bfc4954cf951aa3bbdb22bdf
benebsiny <stu995106@hotmail.com.tw>
2023-05-31 13:39:22 +0800

    `Gauge` component DONE

---
+++ frontend/src/component/Gauge.tsx
@@ -0,0 +1,168 @@
+import React, {FC, useEffect, useRef, useState} from "react";
+
+interface Props {
+    usage: number;
+    maxSupply: number;
+    minSupply: number;
+}
+
+let cur = 0;
+let dir = 1; // 1: clockwise, -1: counter clockwise
+
+const Gauge: FC<Props> = (props) => {
+    let usage = Math.round(props.usage);
+    const maxSupply = Math.round(props.maxSupply);
+    const minSupply = Math.round(props.minSupply);
+    if (usage < minSupply) usage = minSupply;
+    if (usage > maxSupply) usage = maxSupply;
+
+    const [oldProps, setOldProps] = useState<Props>({usage: 700, maxSupply: 1000, minSupply: 0});
+    const gaugeRef = useRef<HTMLCanvasElement>(null);
+    const statsRef = useRef<HTMLDivElement>(null);
+    const minRef = useRef<HTMLDivElement>(null);
+    const maxRef = useRef<HTMLDivElement>(null);
+
+    const oldPercentage = oldProps.usage / (oldProps.maxSupply - oldProps.minSupply);
+    const newPercentage = props.usage / (props.maxSupply - props.minSupply);
+    const diff = Math.abs(newPercentage - oldPercentage);
+
+    useEffect(() => {
+
+        if (oldPercentage === newPercentage) return;
+
+        if (newPercentage > oldPercentage) {
+            dir = 1; cur = 1;
+        } else {
+            dir = -1; cur = 99;
+        }
+
+        movePointer();
+        changeText();
+
+        setOldProps(props);
+    }, [props]);
+
+    function changeText() {
+        if (!statsRef.current || !minRef.current || !maxRef.current) return;
+
+        if (!gaugeRef.current) return;
+        const ctx = gaugeRef.current.getContext("2d");
+        if (!ctx) return;
+
+        statsRef.current.innerText = `${usage}`;
+        minRef.current.innerText = `${minSupply}`;
+        maxRef.current.innerText = `${maxSupply}`;
+
+        if (.7 < newPercentage && newPercentage <= 1.5) statsRef.current.style.color = "#34D5F6";
+        else if (1.5 * Math.PI < newPercentage && newPercentage <= 1.8 * Math.PI) statsRef.current.style.color = "#FFF47D";
+        else statsRef.current.style.color = "#FB6B21";
+    }
+
+    function movePointer() {
+
+        const ctx = gaugeRef.current!.getContext("2d")!;
+        ctx.clearRect(0, 0, gaugeRef.current!.width, gaugeRef.current!.height);
+
+        drawBackground();
+
+        if (dir === 1) drawPointer((oldPercentage + (diff * (cur / 100))) * Math.PI);
+        else drawPointer((newPercentage + (diff * (cur / 100))) * Math.PI);
+
+        if (cur <= 0 || cur >= 100) return;
+        cur += 15 * dir;
+        if (cur > 100) cur = 100;
+        else if (cur < 0) cur = 0;
+
+        requestAnimationFrame(movePointer);
+
+    }
+
+    function drawBackground() {
+        if (!gaugeRef.current) return;
+        const ctx = gaugeRef.current.getContext("2d");
+        if (!ctx) return;
+
+        const centerX = gaugeRef.current.width / 2;
+        const centerY = gaugeRef.current.height / 2;
+
+        /*** DRAW ARC ***/
+        ctx.lineWidth = 20;
+        ctx.shadowBlur = 20;
+
+        // const gradient = ctx.createLinearGradient(0, 0, 0, 170);
+        let gradient = ctx.createConicGradient(0.8 * Math.PI, centerX, centerY);
+        gradient.addColorStop(0, "#34D5F6");
+        gradient.addColorStop(0.3, "#05BBFE");
+        ctx.beginPath();
+        ctx.arc(centerX, centerY, 100, 0.8 * Math.PI, 1.5 * Math.PI);
+        ctx.strokeStyle = gradient;
+        ctx.shadowColor = "#34D5F6";
+        ctx.stroke();
+        ctx.closePath();
+
+        gradient = ctx.createConicGradient(1.5 * Math.PI, centerX, centerY);
+        gradient.addColorStop(0, "#FFF47D");
+        gradient.addColorStop(0.2, "#fce649");
+        ctx.beginPath();
+        ctx.arc(centerX, centerY, 100, 1.5 * Math.PI, 1.8 * Math.PI);
+        ctx.strokeStyle = gradient;
+        ctx.shadowColor = "#FFF47D";
+        ctx.stroke();
+        ctx.closePath();
+
+        gradient = ctx.createConicGradient(1.8 * Math.PI, centerX, centerY);
+        gradient.addColorStop(0, "#FB6B21");
+        gradient.addColorStop(0.2, "#F9491A");
+        ctx.beginPath();
+        ctx.arc(centerX, centerY, 100, 1.8 * Math.PI, 2.2 * Math.PI);
+        ctx.strokeStyle = gradient;
+        ctx.shadowColor = "#FB6B21";
+        ctx.stroke();
+        ctx.closePath();
+    }
+
+    function drawPointer(radian: number) {
+        if (!gaugeRef.current) return;
+        const ctx = gaugeRef.current.getContext("2d");
+        if (!ctx) return;
+
+        const centerX = gaugeRef.current.width / 2;
+        const centerY = gaugeRef.current.height / 2;
+
+        ctx.beginPath();
+        ctx.lineWidth = 3;
+        ctx.shadowColor = "transparent";
+        ctx.moveTo(centerX, centerY);
+        ctx.lineTo(centerX + 100 * Math.cos(radian), centerY + 100 * Math.sin(radian));
+
+        radian %= 2 * Math.PI;
+        if (0.7 * Math.PI <= radian && radian <= 1.5 * Math.PI) ctx.strokeStyle = "#34D5F6";
+        else if (1.5 * Math.PI < radian && radian <= 1.8 * Math.PI) ctx.strokeStyle = "#FFF47D";
+        else ctx.strokeStyle = "#FB6B21";
+
+        ctx.stroke();
+        ctx.closePath();
+    }
+
+    useEffect(() => {
+
+        // let percentage = (usage - minSupply) / (maxSupply - minSupply);
+        // if (percentage < 0) percentage = 0;
+        // if (percentage > 1) percentage = 1;
+
+        drawBackground();
+        drawPointer((0.8 + (1.4 * newPercentage)) * Math.PI);
+
+    }, []);
+
+    return (
+        <div className="relative">
+            <canvas width={250} height={250} ref={gaugeRef}></canvas>
+            <div className="absolute z-50 text-2xl font-bold" style={{top: "60%", left: "50%", transform: "translateX(-50%)"}} ref={statsRef}>456</div>
+            <div className="absolute z-50 text-lg" style={{top: "75%", left: "20%", transform: "translateX(-50%)", color: "#34D5F6"}} ref={minRef}>456</div>
+            <div className="absolute z-50 text-lg" style={{top: "75%", left: "80%", transform: "translateX(-50%)", color: "#F9491A"}} ref={maxRef}>456</div>
+        </div>
+    );
+}
+
+export default Gauge;
--- frontend/src/page/Mainpage.tsx
+++ frontend/src/page/Mainpage.tsx
@@ -1,23 +1,92 @@
-import React, {FC} from "react";
+import React, {FC, MouseEventHandler, RefObject, useEffect, useRef, useState} from "react";
 import "style/mainpage.scss"
-import tw from "assets/tw.svg"
-import { ReactComponent as Logo } from 'assets/tw.svg'
+import {ReactComponent as Logo} from 'assets/tw.svg'
+import Gauge from "component/Gauge";
 
 const Mainpage: FC<any> = () => {
+
+    const [clickedCounty, setClickedCounty] = useState("");
+    const [data, setData] = useState<any>([]);
+
+    const mapRef = useRef() as RefObject<SVGSVGElement>;
+    // const mapRef = useRef<SVGSVGElement|null>(null);
+
+    // @ts-ignore
+    function clicked(event: MouseEvent<HTMLElement, MouseEvent>): MouseEventHandler<SVGSVGElement> {
+        const target = event.target;
+        const countyName = target.getAttribute("name");
+        if (countyName !== null) {
+            setClickedCounty(unicodeToString(countyName)); // Unicode string to string
+        }
+    }
+
+    useEffect(() => {
+        if (!clickedCounty || !mapRef.current) {
+            return;
+        }
+
+        // Color changed
+        mapRef.current.querySelectorAll("path").forEach((path: SVGPathElement) => {
+            if (unicodeToString(path.getAttribute("name") as string) === clickedCounty) {
+                path.classList.add("clicked");
+            } else {
+                path.classList.remove("clicked");
+            }
+        });
+
+        // Set local storage
+        localStorage.setItem("county", clickedCounty);
+
+        // Set data
+
+
+    }, [clickedCounty]);
+
+
+
+    useEffect(() => {
+
+        const storedCounty = localStorage.getItem("county");
+
+        fetch("/api/", {
+            method: "GET"
+        })
+            .then(resp => {
+                if (!resp.ok) throw new Error(resp.statusText);
+                return resp.json();
+            })
+            .then(data => {
+                setData(data);
+                if (storedCounty) {
+                    setClickedCounty(storedCounty);
+                    localStorage.setItem("county", storedCounty);
+                } else {
+                    setClickedCounty("臺北市");
+                    localStorage.setItem("county", "臺北市");
+                }
+            })
+            .catch(err => console.log(err));
+
+    }, []);
+
     return (
         <>
             <div className="flex flex-row">
                 <div>
-                    <div className="flex">
-                        新竹市
-                    </div>
-                    <div>
-                        123
-                    </div>
+                    {clickedCounty && data &&
+                        <>
+                            <h1>{clickedCounty}</h1>
+                            <Gauge
+                                usage={data[clickedCounty].electricity.usage}
+                                maxSupply={data[clickedCounty].electricity.max_supply}
+                                minSupply={data[clickedCounty].electricity.min_supply}
+                            />
+                        </>
+                    }
                 </div>
+
                 <div className="flex-1">
-                    {/*<img src={tw} alt="map"/>*/}
-                    <Logo className="tw-map"/>
+                    <Logo className="tw-map" ref={mapRef} onClick={clicked}/>
                 </div>
             </div>
         </>
@@ -26,3 +95,7 @@ const Mainpage: FC<any> = () => {
 }
 
 export default Mainpage;
+
+function unicodeToString(str: string) {
+    return JSON.parse(`["${str}"]`)[0];
+}
